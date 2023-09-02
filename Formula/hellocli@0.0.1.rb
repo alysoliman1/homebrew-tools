f54328eb2b174b7f13e0c8fffc600663f889b62d
asoliman <aly_soliman@intuit.com>
2023-09-02 00:48:51 -0700

    Formula updated

---
+++ Formula/hellocli@0.0.1.rb
@@ -0,0 +1,14 @@
+class Hellocli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/alysoliman1/cli/archive/refs/tags/v.0.0.1.tar.gz"
+  version "v0.0.1"
+  sha256 "5e1bbb59b207b48da3eefbc8cafb83cbf0d90311da9e4c101692f82d8cc4446c"
+  def install
+    puts Dir.pwd
+  end
+
+  test do
+    hellocli
+  end
+end
