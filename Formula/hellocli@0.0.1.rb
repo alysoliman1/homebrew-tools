dc2c2a1968c09789debcd97fd6624f269925cb09
asoliman <aly_soliman@intuit.com>
2023-09-02 01:26:58 -0700

    cli name changed

--- Formula/hellocli@0.0.1.rb
+++ Formula/hellocli@0.0.1.rb
@@ -1,9 +1,9 @@
 class HellocliAT001 < Formula
   desc ""
   homepage ""
-  url "https://github.com/alysoliman1/cli/archive/refs/tags/v.0.0.1.tar.gz"
+  url "https://github.com/alysoliman1/cli/archive/refs/tags/v.0.0.2.tar.gz"
   version "v0.0.1"
-  sha256 "5e1bbb59b207b48da3eefbc8cafb83cbf0d90311da9e4c101692f82d8cc4446c"
+  sha256"6ea30d3b19b5d6aae18680c6e7fa6706aec88af14eb3421ea64e0619310eac69"
   def install
     puts Dir.entries(".")
     bin.install "bin/hellocli"
