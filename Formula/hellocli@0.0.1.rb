28341aa0efb57e3cf06b3816dbabcf14238321d2
asoliman <aly_soliman@intuit.com>
2023-09-02 01:01:54 -0700

    formula updated

--- Formula/hellocli@0.0.1.rb
+++ Formula/hellocli@0.0.1.rb
@@ -5,7 +5,7 @@ class Hellocli < Formula
   version "v0.0.1"
   sha256 "5e1bbb59b207b48da3eefbc8cafb83cbf0d90311da9e4c101692f82d8cc4446c"
   def install
-    puts Dir.pwd
+    bin.install "builds/hellocli"
   end
   test do
     hellocli
